46e5df08ff6d84793e1c073763479f4f18c0aa84
chaewon-io <0101angela@naver.com>
2024-05-15 18:32:45 +0900

    Test: 부분 조회 적용 테스트 후, rollback

--- src/main/java/com/senity/waved/domain/challenge/repository/ChallengeRepository.java
+++ src/main/java/com/senity/waved/domain/challenge/repository/ChallengeRepository.java
@@ -2,8 +2,6 @@ package com.senity.waved.domain.challenge.repository;
 
 import com.senity.waved.domain.challenge.entity.Challenge;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
-    List<TypeIsFreeLGIProjection> findTypeIsFreeLGIById(Long id);
 }
---
+++ src/main/java/com/senity/waved/domain/member/service/MemberUtil.java
@@ -0,0 +1,23 @@
+package com.senity.waved.domain.member.service;
+
+import com.senity.waved.domain.member.entity.Member;
+import com.senity.waved.domain.member.exception.MemberNotFoundException;
+import com.senity.waved.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MemberUtil {
+    private final MemberRepository memberRepository;
+
+    public Member getMemberById(Long id) {
+        return memberRepository.findById(id)
+                .orElseThrow(() -> new MemberNotFoundException("해당 멤버를 찾을 수 없습니다."));
+    }
+
+    public Member getMemberByEmail(String email) {
+        return memberRepository.findByEmail(email)
+                .orElseThrow(() -> new MemberNotFoundException("해당 멤버를 찾을 수 없습니다."));
+    }
+}
